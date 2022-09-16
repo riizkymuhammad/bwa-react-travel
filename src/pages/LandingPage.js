dc45e8dc7c102f9437902ec3edc15dd896c80601
Muhammad Rizky <mrizky98@gmail.com>
2022-09-17 05:10:20 +0700

    first commit

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../parts/Header'
+import Hero from '../parts/Hero'
+import landingPage from '../json/landingPage.json'
+
+export default class LandingPage extends React.Component {
+    render() {
+        return (
+            <>
+            <Header {...this.props}></Header>
+            <Hero data={landingPage.hero}></Hero>
+            </>
+          )
+    }
+  
+}
